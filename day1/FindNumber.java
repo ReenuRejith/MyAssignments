bf5b66b9b3baa43b353676359959b6548612459e
ReenuRejith <155841683+ReenuRejith@users.noreply.github.com>
2024-01-06 23:22:54 +0530

    Add files via upload
    Day1

---
+++ day1/FindNumber.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int a=45;
+		if (a>0)
+		{System.out.println("The number is positive");}
+		else if (a<0) {System.out.println("The number is negative");}
+		else {System.out.println("The number is neutral");}
+
+	}
+
+}
